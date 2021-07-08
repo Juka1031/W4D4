d02170fa1c3cf9d6055fcdc60d37e21a6562ffb3
julian <juka1031@gmail.com>
2021-07-08 14:59:06 -0400

    stock picker

--- lib/tdd.rb
+++ lib/tdd.rb
@@ -12,14 +12,32 @@ class Array
 
 
    def two_sum
+        if !(self.all? {|ele| ele.is_a?(Integer)})
+            raise "asd"
+        end  
         arr = []
         self.each_with_index do |ele,idx|
             if self.include?(ele*-1)
-                if !arr.include?([ele*-1,idx])
+                if !arr.include?([self.index(ele*-1),idx])
                     arr << [idx,self.index(ele*-1)]
                 end
             end
         end
         arr
    end
+
+   def my_transpose
+        if !(self.all? {|array| array.length == self[0].length})
+            raise "asd"
+        end
+
+        arr = Array.new(self.length) {Array.new(self[0].length)}
+        self.each_with_index do |rows,rows_idx|
+            rows.each_with_index do |ele,ele_idx|
+                arr[ele_idx][rows_idx] = ele
+            end
+        end
+    arr
+   end
 end
+
--- spec/tdd_spec.rb
+++ spec/tdd_spec.rb
@@ -14,6 +14,21 @@ describe Array do
         it 'should return the index of the value that sum to zero' do
             expect(arr.two_sum).to eq([[0,1],[2,4]])
         end
+
+        it "should raise an error if some of the elements are not integers" do
+            expect { array.two_sum }.to raise_error("asd")
+        end
+    end
+
+    describe "#my_transpose" do
+        let(:arr) { [[1,2],[3,4]] }
+        it "should return an array thats been transposed" do
+            expect(arr.my_transpose).to eq ([[1,3],[2,4]])
+        end
+        let(:array) { [[1,2],[3,4,5]] }
+        it "should raise an error if not all the rows have the same number of elements" do
+            expect { array.my_transpose}.to raise_error("asd")
+        end
     end
 end
 
